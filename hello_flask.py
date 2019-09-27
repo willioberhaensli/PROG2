8b7e87ddae0e38a879efa1b36587bac01c741de4
willioberhaensli <55851318+willioberhaensli@users.noreply.github.com>
2019-09-27 15:32:46 +0200

    Flask testen

---
+++ hello_flask.py
@@ -0,0 +1,9 @@
+from flask import flask
+app = Flask("Hello World")
+@app.route('/Hello') 
+def hello_world():
+	return?Hello, World!?
+
+	if __name__ == "__main__":
+		app.run(debug=True, port=5000)
+
