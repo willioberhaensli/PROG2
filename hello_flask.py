fae07f2868c22b9a9d3db32a3ce875c5d11f374c
willioberhaensli <55851318+willioberhaensli@users.noreply.github.com>
2019-10-04 15:27:53 +0200

    Neue Projektidee hochgeladen: Horoskopberechner mit integrierter Datingplattorm.

--- hello_flask.py
+++ hello_flask.py
@@ -1,9 +1,11 @@
-from flask import flask
+from flask import Flask
+from flask import render_template
+
 app = Flask("Hello World")
-@app.route('/Hello') 
+
+@app.route('/hello')
 def hello_world():
-	return'Hello, World!'
+	return render_template('index.html', name="Fabian")
 
-	if __name__ == "__main__":
-		app.run(debug=True, port=5000)
-
+if __name__ == "__main__":
+	app.run(debug=True, port=5000)
