9b97eb7fde4804a83cd9517d2370921038e48ae1
willioberhaensli <55851318+willioberhaensli@users.noreply.github.com>
2019-10-25 13:28:24 +0200

    Ausprobieren, Lernsession mit Mitstudenten

--- hello_flask.py
+++ hello_flask.py
@@ -3,9 +3,7 @@ from flask import render_template
 
 app = Flask("Hello World")
 
-@app.route('/hello')
-def hello_world():
-	return render_template('index.html', name="Fabian")
+
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
