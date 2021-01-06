9ba5467ea1848595b3892b1a5a38600c563f0f60
willioberhaensli <willi.oberhaensli@hotmail.com>
2021-01-06 15:40:44 +0100

    Pflichtfelder Eingabeformular

--- libs/horosdate.py
+++ libs/horosdate.py
@@ -14,7 +14,7 @@ def eintrag_speichern(vorname, nachname, email, telefon, tag, monat, jahr, stund
     return json_daten
 
 
-def klassenkamaraden_lesen():
+'''def klassenkamaraden_lesen():
     data = {}
     try:
         with open('klassenkamaraden.txt', "r") as open_file:
@@ -22,7 +22,7 @@ def klassenkamaraden_lesen():
     except:
         print("Error with file!")
     finally:
-        return data
+        return data'''
 
 def eintrag_speichern_von_formular(form_request):
     print(form_request)
@@ -38,13 +38,11 @@ def eintrag_speichern_von_formular(form_request):
     geburtsort = form_request.get('stadt')
     eintrag_speichern(vorname, nachname, email, telefon, tag, monat, jahr, stunde, minute, stadt)
 
-
+'''
 def person_suchen(form_request):
     daterliste = klassenkamaraden_lesen()
     name = form_request.get('vorname')
 
     if vorname in daterliste:
         return {vorname: daterliste[name]}
-
-    
-
+'''
